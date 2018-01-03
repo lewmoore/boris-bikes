f93c4d98a6624c678156261f024e896981850d2f
rcvink <rrvink@gmail.com>
2018-01-03 15:01:44 +0000

    Add test for bike release

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,4 +2,8 @@ require 'dockingstation.rb'
 
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
+  it 'returns working bike object when bike is released' do
+    expect(subject.release_bike).to be_a_kind_of(Bike)
+    expect(subject.release_bike.working?).to eq true
+  end
 end
