1166f26eb20fb298912c0fe8806af6e9596bce32
rcvink <rrvink@gmail.com>
2018-01-03 17:37:30 +0000

    Add test for no bikes

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,22 +2,16 @@ require 'dockingstation.rb'
 
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
-  it 'returns working bike object when bike is released' do
-    expect(subject.release_bike).to be_a_kind_of(Bike)
-    expect(subject.release_bike.working?).to eq true
-  end
-  # it { is_expected.to respond_to(:dock).with(1).argument }
-  it { is_expected.to respond_to(:bike)}
+  it {is_expected.to respond_to :bike}
   it 'docks something' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
   end
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+  end
 end
-
-
-
-
-# describe Dockingstation.dock(Bike.new) do
-#   it 'stores a bike when a bike is docked' do
-#     expect(Dockingstation.).to
-# end
