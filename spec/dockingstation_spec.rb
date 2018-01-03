d1c50818e09e7a72b8922c638b681818ae7ed59a
Anjana <anjana.lpn@gmail.com>
2018-01-03 16:05:39 +0000

    add tests for storing and reading bikes

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -6,4 +6,18 @@ describe DockingStation do
     expect(subject.release_bike).to be_a_kind_of(Bike)
     expect(subject.release_bike.working?).to eq true
   end
+  # it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:bike)}
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
 end
+
+
+
+
+# describe Dockingstation.dock(Bike.new) do
+#   it 'stores a bike when a bike is docked' do
+#     expect(Dockingstation.).to
+# end
