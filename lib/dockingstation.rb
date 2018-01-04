4582593f677c875650df37b4da893df21ae59488
Lewis Moore <lewis.moore2707@gmail.com>
2018-01-04 15:16:32 +0000

    Added single responsibility to release_bike and dock methods

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -8,13 +8,23 @@ class DockingStation
    end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
+
   def dock(bike)
-    fail "Docking station full" if @bikes.count >= 20
+    fail "Docking station full" if full?
     @bikes << bike
   end
 
+  private
+
+  def empty?
+    return true if @bikes.empty?
+  end
+
+  def full?
+    return true if @bikes.count >= 20
+  end
 end
